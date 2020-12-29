f9a2bed12357ff05186a6b87d018d3b5e2a385ed
barnabywalker <barnaby.walker08@gmail.com>
2020-12-29 12:07:26 +0000

    added lookup function for POWO

--- tests/testthat/test-powo.R
+++ tests/testthat/test-powo.R
@@ -12,6 +12,19 @@ test_that("search URL response is json", {
   expect_equal(httr::http_type(response), "application/json")
 })
 
+test_that("taxon URL response is json", {
+  url <- powo_taxon_url_("30001404-2")
+  response <- httr::GET(url)
+
+  expect_equal(httr::http_type(response), "application/json")
+})
+
+test_that("taxon URL returns 404 for bad ID", {
+  url <- powo_taxon_url_("bad id")
+  response <- httr::GET(url)
+  expect_equal(status_code(response), 404)
+})
+
 test_that("accepted filter only returns accepted names", {
   query <- "Myrcia"
   filters <- c("accepted")
@@ -80,4 +93,25 @@ test_that("image filter only returns things with images", {
                              ~length(.x$images) > 0)
 
   expect_true(all_images)
+})
+
+test_that("lookup with distribution returns distribution field", {
+  taxonid <- "320035-2"
+  results <- lookup_powo(taxonid, distribution=TRUE)
+
+  expect_true("distribution" %in% names(results))
+})
+
+test_that("format search results returns tibble", {
+  results <- search_powo("Poa annua", filters=c("species"))
+  formatted <- format(results)
+
+  expect_s3_class(formatted, "tbl_df")
+})
+
+test_that("format lookup results returns tibble", {
+  results <- lookup_powo("30001404-2")
+  formatted <- format(results)
+
+  expect_s3_class(formatted, "tbl_df")
 })

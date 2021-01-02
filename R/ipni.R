c8a75a96dfa60b575ffacdce09acf2c9db579eda
barnabywalker <barnaby.walker08@gmail.com>
2021-01-02 16:17:27 +0000

    added ipni lookup

--- R/ipni.R
+++ R/ipni.R
@@ -60,6 +60,10 @@
 #' author_results <- search_ipni(list(author_surname="Gardiner"))
 #' format(author_results)
 #'
+#' @family IPNI functions
+#' @seealso
+#'  * [lookup_ipni()] to look up a name using an IPNI ID.
+#'
 #' @export
 search_ipni <- function(query, filters=NULL, limit=50) {
   url <- ipni_search_url_()
@@ -87,9 +91,88 @@ search_ipni <- function(query, filters=NULL, limit=50) {
   )
 }
 
+#' Look up a name in IPNI.
+#'
+#' Request the record for a taxon, author, or publication name in IPNI,
+#' using the IPNI ID.
+#'
+#' The [International Plant Names Index (IPNI)](https://www.ipni.org/)
+#' is a service that provides nomenclatural information for vascular plant names.
+#'
+#' The name lookup API allows users to retrieve information for
+#' a specific taxon, author, or publication name using the unique IPNI ID. If this is not known,
+#' it can be found out using the [IPNI search API][kewr::search_ipni].
+#'
+#' @param id A string containing a valid IPNI ID.
+#' @param type The type of record to look up. Either `taxon`, `author`, or `publication`.
+#'
+#' @return An `ipni_taxon` object, which is a simple structure with fields
+#'   for each of the fields returned by the lookup API, as well as the the [httr response object][httr::response].
+#'
+#' @examples
+#'
+#' # retrieve nomenclatural information for a taxon name
+#' name <- lookup_ipni("271445-2", "taxon")
+#' print(name)
+#'
+#' # format the results in a table
+#' format(name)
+#'
+#' # retrieve nomenclatural information for an author
+#' name <- lookup_ipni("20028192-1", type="author")
+#' format(name)
+#'
+#' # retrieve nomenclatural information for a publication
+#' name <- lookup_ipni("987-2", type="publication")
+#' format(name)
+#'
+#' @family IPNI functions
+#' @seealso
+#'  * [search_ipni()] to search IPNI using a taxon name.
+#'
+#' @export
+lookup_ipni <- function(id, type=c("taxon", "author", "publication")) {
+  type <- match.arg(type)
+
+  url <- ipni_lookup_url_(id, type)
+
+  result <- make_request_(url, query=NULL)
+
+  # this might be better if things were explicitly listed
+  record <- result$content
+  record$response <- result$response
+  record$queryId <- id
+
+  structure(
+    record,
+    class="ipni_name"
+  )
+}
+
+#' Make the IPNI search URL.
+#'
 #' @noRd
 ipni_search_url_ <- function() {
   base <- get_url_("ipni")
 
   paste0(base, "/search")
+}
+
+#' Make the IPNI name lookup URL.
+#'
+#' @param id A valid IPNI ID.
+#' @param type The type of name to lookup.
+#'
+#' @noRd
+#'
+#' @importFrom glue glue
+ipni_lookup_url_ <- function(id, type) {
+  base <- get_url_("ipni")
+
+  type <- switch(type,
+                 taxon="n",
+                 author="a",
+                 publication="p")
+
+  glue("{base}/{type}/{id}")
 }

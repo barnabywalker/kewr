04d71308eeac00c27459bfe0e93de6aa3b8f5549
barnabywalker <barnaby.walker08@gmail.com>
2021-02-02 15:22:19 +0000

    added waits before test powo requests

--- tests/testthat/test-powo.R
+++ tests/testthat/test-powo.R
@@ -1,5 +1,7 @@
 test_that("search URL returns status 200", {
   url <- powo_search_url_()
+
+  Sys.sleep(0.1)
   response <- httr::GET(url)
 
   expect_equal(httr::status_code(response), 200)
@@ -7,6 +9,8 @@ test_that("search URL returns status 200", {
 
 test_that("search URL response is json", {
   url <- powo_search_url_()
+
+  Sys.sleep(0.1)
   response <- httr::GET(url)
 
   expect_equal(httr::http_type(response), "application/json")
@@ -14,6 +18,8 @@ test_that("search URL response is json", {
 
 test_that("taxon URL response is json", {
   url <- powo_taxon_url_("30001404-2")
+
+  Sys.sleep(0.1)
   response <- httr::GET(url)
 
   expect_equal(httr::http_type(response), "application/json")
@@ -21,13 +27,17 @@ test_that("taxon URL response is json", {
 
 test_that("taxon URL returns 404 for bad ID", {
   url <- powo_taxon_url_("bad id")
+
+  Sys.sleep(0.1)
   response <- httr::GET(url)
+
   expect_equal(status_code(response), 404)
 })
 
 test_that("raises error for unimplemented keyword", {
   query <- list(published="1920")
 
+  Sys.sleep(0.1)
   expect_error(search_powo(query),
                "Query keyword.+ not recognised")
 })
@@ -36,6 +46,7 @@ test_that("accepted filter only returns accepted names", {
   query <- "Myrcia"
   filters <- c("accepted")
 
+  Sys.sleep(0.1)
   results <- search_powo(query, filters)
   all_accepted <- purrr::every(results$results,
                                ~.x$accepted)
@@ -47,6 +58,7 @@ test_that("specific filter only returns species", {
   query <- "Myrcia"
   filters <- c("species")
 
+  Sys.sleep(0.1)
   results <- search_powo(query, filters)
   all_species <- purrr::every(results$results,
                               ~.x$rank == "Species")
@@ -58,6 +70,7 @@ test_that("generic filter only returns genera", {
   query <- "Myrcia"
   filters <- c("genera")
 
+  Sys.sleep(0.1)
   results <- search_powo(query, filters)
   all_genera <- purrr::every(results$results,
                              ~.x$rank == "Genus")
@@ -71,6 +84,7 @@ test_that("infraspecific filter only returns infraspecifics", {
   query <- "Poa annua"
   filters <- c("infraspecies")
 
+  Sys.sleep(0.1)
   results <- search_powo(query, filters)
   all_infra <- purrr::every(results$results,
                             ~.x$rank %in% infra_ranks)
@@ -83,6 +97,7 @@ test_that("family filter only returns families", {
   query <- "poaceae"
   filters <- c("families")
 
+  Sys.sleep(0.1)
   results <- search_powo(query, filters)
   all_families <- purrr::every(results$results,
                             ~.x$rank == "Family")
@@ -95,6 +110,7 @@ test_that("image filter only returns things with images", {
   query <- "Myrcia"
   filters <- c("has_images")
 
+  Sys.sleep(0.1)
   results <- search_powo(query, filters)
   all_images <- purrr::every(results$results,
                              ~length(.x$images) > 0)
@@ -104,12 +120,15 @@ test_that("image filter only returns things with images", {
 
 test_that("lookup with distribution returns distribution field", {
   taxonid <- "320035-2"
+
+  Sys.sleep(0.1)
   results <- lookup_powo(taxonid, distribution=TRUE)
 
   expect_true("distribution" %in% names(results))
 })
 
 test_that("format search results returns tibble", {
+  Sys.sleep(0.1)
   results <- search_powo("Poa annua", filters=c("species"))
   formatted <- format(results)
 
@@ -117,6 +136,7 @@ test_that("format search results returns tibble", {
 })
 
 test_that("format lookup results returns tibble", {
+  Sys.sleep(0.1)
   results <- lookup_powo("30001404-2")
   formatted <- format(results)
 
@@ -125,7 +145,11 @@ test_that("format lookup results returns tibble", {
 
 test_that("cursor returns next page of resutls", {
   query <- list(genus="Ulex")
+
+  Sys.sleep(0.1)
   page1 <- search_powo(query)
+
+  Sys.sleep(0.1)
   page2 <- search_powo(query, cursor=page1$cursor)
 
   expect_false(page1$results[[1]]$fqId == page2$results[[1]]$fqId)

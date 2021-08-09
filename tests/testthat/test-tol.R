537794d71f214c3d14b859f40597ff30cabf9a7f
barnabywalker <barnaby.walker08@gmail.com>
2021-08-09 23:44:20 +0100

    added tests for ToL search

--- tests/testthat/test-tol.R
+++ tests/testthat/test-tol.R
@@ -1,3 +1,33 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("search URL returns status 200", {
+  url <- tol_search_url_()
+  response <- httr::GET(url)
+
+  expect_equal(httr::status_code(response), 200)
+})
+
+test_that("search URL response is json", {
+  url <- tol_search_url_()
+  response <- httr::GET(url)
+
+  expect_equal(httr::http_type(response), "application/json")
+})
+
+test_that("raises error for keyword search", {
+  query <- list(name="Myrcia guianensis")
+
+  expect_error(search_tol(query),
+               "Keyword-based search not implemented")
+})
+
+test_that("raises error for bad query input type", {
+  query <- c("this", "is", "a", "bad", "query")
+
+  expect_error(search_tol(query))
+})
+
+test_that("tidy search results returns tibble", {
+  results <- search_tol("Poa annua")
+  tidied <- tidy(results)
+
+  expect_s3_class(tidied, "tbl_df")
 })
